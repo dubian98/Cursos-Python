0f2b66399703ec331ebbabe4859c40a29924f83a
Dubian Gomez <dubian.gomez98@gmail.com>
2021-04-03 20:59:12 -0500

    videos 38

--- manejo_archivos.py
+++ manejo_archivos.py
@@ -37,3 +37,32 @@ archivo_texto = open("archivo.txt","a")
 archivo_texto.write("\nsiempre es una buena ocacion para estudiar python")
 archivo_texto.close()
 
+#punteros 
+from io import open 
+
+archivo_texto = open("archivo.txt","r+") #lectura y escritura
+
+print(archivo_texto.read(11))
+
+archivo_texto.seek(30)
+archivo_texto.write("comienzo del texto\n")
+print(archivo_texto.read())
+
+
+#agregar una linea
+
+from io import open 
+
+archivo_texto = open("archivo.txt","r+") #lectura y escritura
+
+lista_texto = archivo_texto.readlines()
+
+lista_texto[2] = "333333333333\n"
+
+archivo_texto.seek(0)
+
+archivo_texto.writelines(lista_texto)
+archivo_texto.seek(0)
+print(archivo_texto.read())
+
+archivo_texto.close()
